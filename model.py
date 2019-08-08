0f47edca52780b764ff6bc0d5dea8afc64ca5d4b
ayeshamanjura <ayesha.manjura00@gmail.com>
2019-08-08 16:16:22 -0400

    edit emetro in fastie.py

--- model.py
+++ model.py
@@ -3,4 +3,6 @@ from google.appengine.ext import ndb
 class Emetrocard(ndb.Model):
     Emetrocard_line1 = ndb.StringProperty(required=True)
     Emetrocard_line2 = ndb.StringProperty(required=True)
-    Emetrocard_type = ndb.StringProperty(required=True)
+    Emetrocard_choice = ndb.StringProperty(required=True)
+    Emetrocard_balance = ndb.FloatProperty(required= True)
+    url= ndb.StringProperty(required=True, default="https://upload.wikimedia.org/wikipedia/en/8/8b/MetroCard.SVG")

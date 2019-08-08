2f3696d6d026cb3d450e86dad8e6dc0d56286e55
ayeshamanjura <ayesha.manjura00@gmail.com>
2019-08-08 16:17:22 -0400

    changes made in Fastie.py

--- Fastie.py
+++ Fastie.py
@@ -9,51 +9,48 @@ the_jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-def get_Emetrocard():
-    Emetrocard_type=["show your current balance"]
-    return(random.choice(Emetrocard_type))
-
-class ShowbalanceHandler(object):
-    def __init__(self, balance):
-        self.balance = balance
-
-        self.response.write('this is your current balance in the card')
-
-    def __str__(self):
-        return("the balance in the account %s USD" %( self.balance))
-
-    def deposit(self, d_amount):
-        if d_amount < 0:
-            print("add balance into your card")
-        else:
-            self.balance = self.balance + d_amount
+class welcomeHandler(webapp2.requestHandler):
+    def get(self):
+        welcome_template = the_jinja_env.get_template('templates/welcome.html')
+        self.response.write(welcome_template.render())
 
 class EmetrocardHandler(webapp2.requestHandler):
     def get(self):
         Emetrocard_template = the_jinja_env.get_template('templates/E-metrocard.html')
 
-        self.response.write(e-metrocard_template.render())
+        self.response.write(Emetrocard_template.render())
 
     def post(self):
         Emetrocard_card_number = self.request.get('user-card-number')
         Emetrocard_card_expiration_date = self.request.get('user-expiration-date')
-        Emetrocard_img_choice = self.request.get('Emetrocard-type')
+        Emetrocard_choice = self.request.get('Emetrocard-type')
 
-        user_Emetrocard = Emetrocatd(
-            Emetrocard_line1 = Emetrocard_first_line,
-            Emetrocard_line2 = Emetrocard_second_line,
-            pic_type = Emetrocard_img_choice
-
+        user_Emetrocard = Emetrocard(
+            Emetrocard_line1 = Emetrocard_card_number,
+            Emetrocard_line2 = Emetrocard_expiration_date,
+            Emetrocard_choice = Emetrocard_choice,
+            Emetrocard_balance = random.choice()
         )
-
         user_Emetrocard.put()
+
+    def deposit(self, d_amount):
+        if d_amount < 0:
+            print("add balance into your card")
+        else:
+            self.Emetrocard_balance = self.Emetrocard_balance + d_amount
+            user_Emetrocard.put()
 
 class serviceHandler(webapp2.requestHandler):
     def get(self):
         service_template = the_jinja_env.get_template('templates/trainservices.html')
 
+class ScheduleHandler(webapp2.requestHandler):
+    def get(self):
+        schedule_template = the_jinja_env.get_template('templates/schedule.html')
+
 app = webapp2.WSGIApplication([
     ('/', EmetrocardHandler),
     ('/Emetrocard', ShowbalanceHandler),
     ('/service', serviceHandler)
+    ('/schedule' scheduleHandler)
 ], debug=True)

7586f3b955d818f37d53a68ce1853e526e1db669
Jibrael Yilmaz <yilmazjibrael@gmail.com>
2019-08-08 09:55:45 -0400

    created button

---
+++ fastie.js
@@ -0,0 +1,15 @@
+const welcomeButton = document.querySelector('#button');
+
+welcomeButton.addEventListener('click', (event) =>{
+
+//
+console.log('Like button clicked!');
+  welcomeButton.style.fontSize = "200%";
+  // likeButton.innerHTML += "d!!!";
+  // likeButton.style.backgroundColor = "pink";
+
+
+
+
+
+});
